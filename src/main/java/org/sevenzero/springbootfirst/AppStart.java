64251245722ef3e25e92ec67b4f035b366088dd0
libin <libin2@ctsi.com.cn>
2017-05-10 14:18:25 +0800

    dev first

---
+++ src/main/java/org/sevenzero/springbootfirst/AppStart.java
@@ -0,0 +1,20 @@
+package org.sevenzero.springbootfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class AppStart {
+	
+	public static void main(String[] args) throws Exception {
+		
+		SpringApplication.run(AppStart.class, args);
+		
+	}
+	
+}
---
+++ src/main/java/org/sevenzero/springbootfirst/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package org.sevenzero.springbootfirst.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+}
