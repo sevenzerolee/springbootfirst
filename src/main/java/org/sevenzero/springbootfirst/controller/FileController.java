eb468b4a14a5ec8d435892851b816b95c057f123
libin <libin2@ctsi.com.cn>
2017-06-07 16:14:08 +0800

    多文件上传

--- src/main/java/org/sevenzero/springbootfirst/controller/FileController.java
+++ src/main/java/org/sevenzero/springbootfirst/controller/FileController.java
@@ -5,6 +5,9 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Controller
 @RequestMapping("/file")
@@ -23,6 +27,51 @@ public class FileController {
 	public String uploadIndex() {
 		
 		return "/file/upload";
+	}
+	
+	@RequestMapping("uploadmult")
+	@ResponseBody
+	public String uploadMult(HttpServletRequest request) {
+		List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("file");
+		log.info("## " + files.size());
+		
+		int count = 0;
+		for (MultipartFile file : files) {
+			if (!file.isEmpty()) {
+				log.info("## 文件不为空 " + file.getOriginalFilename());
+				String uploadDir = "/home/lb/temp/upload";
+				File fileDir = new  File(uploadDir);
+				if (!fileDir.exists()) {
+					fileDir.mkdirs();
+				}
+				String path = uploadDir + File.separator + file.getOriginalFilename();
+				
+				try ( BufferedInputStream bis = new BufferedInputStream(file.getInputStream());
+						BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path)) ) {
+					
+					byte[] buffer = new byte[2048];
+					int size = -1;
+					while ( -1 != (size = bis.read(buffer, 0, 2048)) ) {
+						bos.write(buffer, 0, size);
+					}
+					bos.flush();
+					log.info("保存文件 " + path);
+				} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+				count++;
+			}
+			else {
+				log.info("## 文件为空");
+			}
+		}
+		log.info("## count " + count);
+		if (count == files.size()) {
+			return "成功";
+		}
+		
+		return "失败";
 	}
 	
 	@RequestMapping("upload")
@@ -38,7 +87,7 @@ public class FileController {
 			String path = uploadDir + File.separator + file.getOriginalFilename();
 			
 			try ( BufferedInputStream bis = new BufferedInputStream(file.getInputStream());
-					BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path))) {
+					BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path)) ) {
 				
 				byte[] buffer = new byte[2048];
 				int size = -1;
@@ -51,11 +100,14 @@ public class FileController {
 			catch (IOException e) {
 				e.printStackTrace();
 			}
+			
+			return "成功";
 		}
 		else {
 			log.info("## 文件为空");
+			
+			return "失败";
 		}
-		return "成功或失败";
 	}
 
 }
